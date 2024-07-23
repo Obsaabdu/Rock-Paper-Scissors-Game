a903f80d0b3ee709da75baafc16d12c7fd093561
Obsaabdu <obsaabdu01@gmail.com>
2024-07-23 22:01:33 +0300

    modified javascript and now it is full

--- script.js
+++ script.js
@@ -1,33 +1,48 @@
-function getComputerChoice() {
-    const choice = ["Rock", "Paper", "Scissors"];
-    const random  = Math.floor(Math.random()*choice.length);
-    return choice[random];
-}
-function theGame() {
-    const choice = document.getElementById("userInput").value;
-    const result = document.getElementById("result");
-    const rand = document.getElementById("comp");
-    comp = getComputerChoice()
-    
+document.addEventListener("DOMContentLoaded", () => {
+  const user = document.querySelector(".userchoice");
+  const computer = document.querySelector(".computerchoice");
+  const result = document.querySelector(".result");
+  const choices = document.querySelectorAll(".choice");
 
-    rand.textContent = "My choice "+ comp +"."; 
-    let r 
-    if (choice === "Paper"){
-        if (comp === "Rock") r = "I WIN!\nGame Over!!";
-        else if (comp === "Scissors") r = "YOU WIN!\nCongratulations!!";
-        else r = "It's Draw!!";
-    }
-    else if (choice === "Rock"){
-        if (comp === "Paper") r = "I WIN!\nGame Over!!";
-        else if (comp === "Scissors") r = "YOU WIN!\nCongratulations!!";
-        else r = "It's Draw!!";
-    }
-    else if (choice === "Scissors"){
-        if (comp === "Rock") r = "I WIN!\nGame Over!!";
-        else if (comp === "Paper") r = "YOU WIN!\nCongratulations!!";
-        else r = "It's Draw!!";
-    }   
-    result.textContent = r;
-}
+  let userChoice;
+  let computerChoice;
+  const choicesArray = ["rock", "paper", "scissors"];
 
-theGame()
+  choices.forEach((choice) =>
+    choice.addEventListener("click", (e) => {
+      userChoice = e.target.alt;
+      user.src = `images/${userChoice}.png`;
+      generateComputerChoice();
+      getResult();
+    })
+  );
+
+  function generateComputerChoice() {
+    computerChoice =
+      choicesArray[Math.floor(Math.random() * choicesArray.length)];
+    computer.src = `images/${computerChoice}.png`;
+  }
+  function getResult() {
+    if (userChoice === computerChoice) {
+      result.textContent = "Match Draw";
+    } else if (userChoice === "rock") {
+      if (computerChoice === "paper") {
+        result.textContent = "CPU Won!!";
+      } else {
+        result.textContent = "You Win!!";
+      }
+    } else if (userChoice === "paper") {
+      if (computerChoice === "scissors") {
+        result.textContent = "CPU Won!!";
+      } else {
+        result.textContent = "You Win!!";
+      } 
+    } else {
+      if (computerChoice === "rock") {
+        result.textContent = "CPU Won!!";
+      } else {
+        result.textContent = "You Win!!";
+      }
+    }
+  }
+});
